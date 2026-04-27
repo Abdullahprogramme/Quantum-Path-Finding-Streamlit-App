1b0affff36a07f74d20fc731f842659249ce5e37
Abdullah Tariq <111647623+Abdullahprogramme@users.noreply.github.com>
2026-04-27 18:19:15 +0500

    Use ordered neighbors in A* algorithm

--- algorithms/a_star.py
+++ algorithms/a_star.py
@@ -74,7 +74,7 @@ def a_star(grid_manager: GridManager) -> AStar_Result:
 
             return AStar_Result(path, time_taken, nodes_explored, visited)
         
-        for neighbor in grid_manager.neighbors(current):
+        for neighbor in grid_manager.neighbors_with_order(current, ('U', 'D', 'L', 'R')):
             if neighbor in closed_set:
                 continue
 
